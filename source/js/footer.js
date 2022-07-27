ab9dd46de403a9a1c4441bf834265fea1de78813
Stillrain577681223 <95129294+Stillrain577681223@users.noreply.github.com>
2022-07-27 21:35:41 +0800

    Add files via upload

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2022 Stillrain</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
